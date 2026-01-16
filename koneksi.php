b28d9eefd1c17f860cf065dee0412bd51f9f98d8
rhmadelya <ptrdelyaa.99@gmail.com>
2026-01-16 13:16:24 +0700

    Menambahkan fitur baru / memperbaiki bug

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "webdailyjournal";
+$servername = "sql308.infinityfree.com";
+$username = "if0_40857713";
+$password = "HbmBPOKfUQhUOOB";
+$db = "if0_40857713_dailyjournal";
 
 //create connection
 $conn = new mysqli($servername,$username,$password,$db);
